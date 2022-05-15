e7ea6e0b6c142af81a0834d1abf2f1b1607481fb
xJaimeGx <beanieman019@protonmail.com>
2022-05-15 16:07:23 -0500

    change file path and recode About and Nav comp

---
+++ src/components/About.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function About() {
+    return (
+        <div>
+            <p className="about-info">Front-End Web Developer</p>
+            <img className="profile-pic" src={process.env.PUBLIC_URL + 'src/assets/images/profile-pic.jpeg'} alt="Photo of Jaime Gutierrez" />
+            <p className="about-info">
+                My name is Jaime Gutierrez and i am excited to start a new career in
+                front-end web development!
+            </p>
+        </div>
+    )
+};
+
+export default About;
---
+++ src/components/Nav.js
@@ -0,0 +1,25 @@
+import React from "react";
+
+function Nav(props) {
+    const navOption = ["About"];
+    return (
+        <div className="h-link">
+            <ul className="list-link">
+                {navOption.map((link) => (
+                    <li className={props.currentPage === link ? "nav-option is-active" : "nav-option"}
+                    key={link}
+                    >
+                        <a href={"#" + link.toLowerCase()}
+                           onClick={() => props.handlePageChange(link)}
+                           className={props.currentPage === link ? "nav-page is-active" : "nav-page"}
+                        >
+                            {link}
+                        </a>
+                    </li>
+                ))}
+            </ul>
+        </div>
+    )
+};
+
+export default Nav;
